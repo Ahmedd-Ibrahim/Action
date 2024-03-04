8419a85ce0b49f13254f6a56d06437dbcaf1bc3b
Ahmedd-Ibrahim <0ahmedibrahem@gmail.com>
2024-03-04 18:50:31 +0200

    clean code

--- src/Console/Commands/MakeActionCommand.php
+++ src/Console/Commands/MakeActionCommand.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Str;
 class MakeActionCommand extends Command
 {
     protected $signature = 'action:make {name}';
-
     protected $description = 'Generate a new action class';
 
     public function handle()
@@ -16,15 +15,20 @@ class MakeActionCommand extends Command
         $this->createAction();
     }
 
-    protected function createAction()
+    public function createAction()
     {
-        $name = $this->argument('name');
+        $name = str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $this->argument('name'));
         $className = Str::studly($name) . 'Action';
         $interfaceName = Str::studly(basename($name));
-        $interfaceWithNamespace = contract_prefix() . DIRECTORY_SEPARATOR . Str::studly($name);
-        $classNamespace = action_prefix() . DIRECTORY_SEPARATOR . get_namespace_from_class_name($className);
+        $interfaceNameWithPrefix = Str::studly($name);
+        $interfaceWithNamespace = contract_prefix() . DIRECTORY_SEPARATOR . $interfaceNameWithPrefix;
+        $classNamespace = action_prefix();
 
-        $interfacePath = app_path(get_contracts_path() . DIRECTORY_SEPARATOR . basename($interfaceWithNamespace) . '.php');
+        if (!blank(get_namespace_from_class_name($className))) {
+            $classNamespace .= DIRECTORY_SEPARATOR . get_namespace_from_class_name($className);
+        }
+
+        $interfacePath = app_path(get_contracts_path() . DIRECTORY_SEPARATOR . $interfaceNameWithPrefix . '.php');
         $classPath = app_path(get_actions_path() . DIRECTORY_SEPARATOR . $className . '.php');
 
         $this->createDirectoryIfNotExists(dirname($interfacePath));
@@ -35,33 +39,24 @@ class MakeActionCommand extends Command
             return;
         }
 
-        $classContent = $this->generateClassContent($className, $interfaceName, $interfaceWithNamespace, $classNamespace);
-        $interfaceContent = $this->generateInterfaceContent($interfaceName, $interfaceWithNamespace);
+        $classContent = file_get_contents(__DIR__ . '/stubs/action.stub');
+        $classContent = str_replace(
+            ['{{className}}', '{{interfaceName}}', '{{interfaceNameSpace}}', '{{classNamespace}}'],
+            [basename($className), basename($interfaceName), $interfaceWithNamespace, $classNamespace],
+            $classContent
+        );
+
+        $interfaceContent = file_get_contents(__DIR__ . '/stubs/interface.stub');
+        $interfaceContent = str_replace(
+            ['{{interfaceName}}', '{{interfaceNameSpace}}'],
+            [basename($interfaceName), dirname($interfaceWithNamespace)],
+            $interfaceContent
+        );
 
         file_put_contents($classPath, $classContent);
         file_put_contents($interfacePath, $interfaceContent);
 
         $this->info('Action created successfully: ' . $className);
-    }
-
-    private function generateClassContent($className, $interfaceName, $interfaceWithNamespace, $classNamespace)
-    {
-        $classStub = file_get_contents(__DIR__ . '/stubs/action.stub');
-        return str_replace(
-            ['{{className}}', '{{interfaceName}}', '{{interfaceNameSpace}}', '{{classNamespace}}'],
-            [$className, $interfaceName, $interfaceWithNamespace, $classNamespace],
-            $classStub
-        );
-    }
-
-    private function generateInterfaceContent($interfaceName, $interfaceWithNamespace)
-    {
-        $interfaceStub = file_get_contents(__DIR__ . '/stubs/interface.stub');
-        return str_replace(
-            ['{{interfaceName}}', '{{interfaceNameSpace}}'],
-            [$interfaceName, $interfaceWithNamespace],
-            $interfaceStub
-        );
     }
 
     private function createDirectoryIfNotExists($path)
